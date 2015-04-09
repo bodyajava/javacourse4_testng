bace51d45745662523fd9f61900cb315cf920069
bodya48 <bogdanlaukhin@MacBook-Pro-Bogdan.local>
2015-04-09 21:46:30 +0300

    Home task #4

--- src/test/java/home/pages/FileCreationTests.java
+++ src/test/java/home/pages/FileCreationTests.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
